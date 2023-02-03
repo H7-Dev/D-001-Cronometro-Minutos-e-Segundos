3d4b3c5405b5883f67972684d52eaaf1a68aadbc
Herbert <herbert.dev.alfa@gmail.com>
2023-02-03 08:17:55 -0300

    01.05 [-g implemento] implemento a partir da variável "maxMinutes " um tempo máximo

--- ASSETS/JS/script.js
+++ ASSETS/JS/script.js
@@ -1,9 +1,22 @@
-// renomear repositório
+// Este é o código JavaScript que cria um cronômetro. O cronômetro exibe os minutos e segundos decorridos na página e possui as seguintes funções: iniciar, pausar, reiniciar e adicionar uma volta.
+
+// A função startTimer() inicia o cronômetro definindo um intervalo que é executado a cada segundo, atualizando as exibições de minutos e segundos e alternando a visibilidade dos botões de início e pausa.
+
+// A função pauseTimer() interrompe o intervalo, pausando efetivamente o cronômetro.
+
+// A função resetTimer() redefine o cronômetro para seu estado inicial: 0 minutos, 0 segundos, com o visor e o histórico de voltas limpos e o botão Iniciar exibido e o botão Pausa oculto.
+
+// A função addLapAndCalculateAverage() registra o tempo atual como uma volta, adiciona-o ao histórico de voltas e calcula o tempo médio de todas as voltas. O tempo médio é exibido na página.
+
+// Os ouvintes de evento são adicionados aos botões para vincular cada função aos respectivos cliques de botão.
+
+
 let minute = 0;
 let second = 0;
 let intervalId = null;
 let lapCount = 0;
 let totalSeconds = 0;
+let maxMinutes = 1;
 
 const minuteDisplay = document.querySelector("#minute");
 const secondDisplay = document.querySelector("#second");
@@ -14,13 +27,17 @@ const resetButton = document.querySelector("#reset");
 const lapButton = document.querySelector("#lap");
 const lapList = document.querySelector("#laps");
 
-// Função para iniciar o cronômetro
 function startTimer() {
   intervalId = setInterval(() => {
     second++;
     if (second === 60) {
       minute++;
       second = 0;
+    }
+    if (minute === maxMinutes) {
+      clearInterval(intervalId);
+      alert("Tempo esgotado!");
+      return;
     }
     minuteDisplay.textContent = (minute < 10 ? "0" : "") + minute;
     secondDisplay.textContent = (second < 10 ? "0" : "") + second;
@@ -29,14 +46,12 @@ function startTimer() {
   pauseButton.style.display = "inline-block";
 }
 
-// Função para pausar o cronômetro
 function pauseTimer() {
   clearInterval(intervalId);
   startButton.style.display = "inline-block";
   pauseButton.style.display = "none";
 }
 
-// Função para resetar o cronômetro
 function resetTimer() {
   clearInterval(intervalId);
   minute = 0;
@@ -51,29 +66,8 @@ function resetTimer() {
   pauseButton.style.display = "none";
 }
 
-// Função para adicionar uma lap
-function addLap() {
-  lapCount++;
-  totalSeconds += minute * 60 + second;
-  const minuteString = (minute < 10 ? "0" : "") + minute;
-  const secondString = (second < 10 ? "0" : "") + second;
-  const lap = `${minuteString}:${secondString}`;
-  const lapItem = document.createElement("li");
-  lapItem.textContent = lap;
-  lapList.appendChild(lapItem);
-
-  const averageSeconds = Math.floor(totalSeconds / lapCount);
-  const averageMinutes = Math.floor(averageSeconds / 60);
-  const remainingSeconds = averageSeconds % 60;
-  const averageMinuteString = (averageMinutes < 10 ? "0" : "") + averageMinutes;
-  const averageSecondString = (remainingSeconds < 10 ? "0" : "") + remainingSeconds;
-  const averageTime = `${averageMinuteString}:${averageSecondString}`;
-  averageDisplay.textContent = averageTime;
-}
-
 let lapTimes = [];
 
-// Função para adicionar uma lap e calcular a média
 function addLapAndCalculateAverage() {
   const minuteString = (minute < 10 ? "0" : "") + minute;
   const secondString = (second < 10 ? "0" : "") + second;
@@ -94,7 +88,6 @@ function addLapAndCalculateAverage() {
   const averageSecondString = (averageSecond < 10 ? "0" : "") + averageSecond;
   document.querySelector("#media").textContent = 'Média ' +`${averageMinuteString}:${averageSecondString}`;
 }
-
 
 startButton.addEventListener("click", startTimer);
 pauseButton.addEventListener("click", pauseTimer);
--- README.md
+++ README.md
@@ -6,6 +6,106 @@
 <!-- ✔️  -->
 <!--  ✅    -->
 <!--  ▶️    -->
+
+### **🏁❗  01.05 [-g implemento] implemento a partir da variável "maxMinutes " um tempo máximo**
+
+> #### **Descrição**
+>
+>     implementa uma variável que limita o tempo máximo x minutos.
+>
+> ---
+>#### ***✍️Exemplo de código JS***
+>``` JS
+>
+>let minute = 0;
+>let second = 0;
+>let intervalId = null;
+>let lapCount = 0;
+>let totalSeconds = 0;
+>let maxMinutes = 1;
+>
+>const minuteDisplay = document.querySelector("#minute");
+>const secondDisplay = document.querySelector("#second");
+>const averageDisplay = document.querySelector("#media");
+>const startButton = document.querySelector("#play");
+>const pauseButton = document.querySelector("#pause");
+>const resetButton = document.querySelector("#reset");
+>const lapButton = document.querySelector("#lap");
+>const lapList = document.querySelector("#laps");
+>
+>function startTimer() {
+>  intervalId = setInterval(() => {
+>    second++;
+>    if (second === 60) {
+>      minute++;
+>      second = 0;
+>    }
+>    if (minute === maxMinutes) {
+>      clearInterval(intervalId);
+>      alert("Tempo esgotado!");
+>      return;
+>    }
+>    minuteDisplay.textContent = (minute < 10 ? "0" : "") + minute;
+>    secondDisplay.textContent = (second < 10 ? "0" : "") + second;
+>  }, 1000);
+>  startButton.style.display = "none";
+>  pauseButton.style.display = "inline-block";
+>}
+>
+>function pauseTimer() {
+>  clearInterval(intervalId);
+>  startButton.style.display = "inline-block";
+>  pauseButton.style.display = "none";
+>}
+>
+>function resetTimer() {
+>  clearInterval(intervalId);
+>  minute = 0;
+>  second = 0;
+>  lapCount = 0;
+>  totalSeconds = 0;
+>  minuteDisplay.textContent = "00";
+>  secondDisplay.textContent = "00";
+>  averageDisplay.textContent = "00:00";
+>  lapList.innerHTML = "";
+>  startButton.style.display = "inline-block";
+>  pauseButton.style.display = "none";
+>}
+>
+>let lapTimes = [];
+>
+>function addLapAndCalculateAverage() {
+>  const minuteString = (minute < 10 ? "0" : "") + minute;
+>  const secondString = (second < 10 ? "0" : "") + second;
+>  const lap = `${minuteString}:${secondString}`;
+>  const lapItem = document.createElement("li");
+>  lapItem.textContent = lap;
+>  lapList.appendChild(lapItem);
+>
+>  lapTimes.push(minute * 60 + second);
+>  let totalTime = 0;
+>  lapTimes.forEach((time) => {
+>    totalTime += time;
+>  })
+>  const averageTime = Math.round(totalTime / lapTimes.length);
+>  const averageMinute = Math.floor(averageTime / 60);
+>  const averageSecond = averageTime % 60;
+>  const averageMinuteString = (averageMinute < 10 ? "0" : "") + averageMinute;
+>  const averageSecondString = (averageSecond < 10 ? "0" : "") + averageSecond;
+>  document.querySelector("#media").textContent = 'Média ' +`${averageMinuteString}:$>{averageSecondString}`;
+>}
+>
+>startButton.addEventListener("click", startTimer);
+>pauseButton.addEventListener("click", pauseTimer);
+>resetButton.addEventListener("click", resetTimer);
+>lapButton.addEventListener("click", addLapAndCalculateAverage);
+>
+>```
+>
+>
+
+
+
 ### **🏁❗  01.04 [-g implemento] função para addLap e calcular a média das votlas**
 
 > #### **Descrição**
